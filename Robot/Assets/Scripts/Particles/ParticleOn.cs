98e67e7112c65cfc7db728afa3fd63a500a7fb40
Blast2Slay <38786844+Blast2Slay@users.noreply.github.com>
2018-06-11 13:20:09 +0100

    Visual Light
    A visual light that activates as the players move along the path to the next puzzle

---
+++ Robot/Assets/Scripts/Particles/ParticleOn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleOn : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player1")
+        {
+            target.SetActive(true);
+        }
+
+        if (col.gameObject.tag == "Player2")
+        {
+            target.SetActive(true);
+        }
+
+    }
+}

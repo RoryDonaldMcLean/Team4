5939c89ff89a0670f8ccc718b3e2c8faa2c13ecc
xyskin <xyskin@163.com>
2018-06-19 15:25:55 +0100

    implement limbos parabola and slightly change the codings(LimsFly.cs)

--- Robot/Assets/Scripts/Player/LimsFly.cs
+++ Robot/Assets/Scripts/Player/LimsFly.cs
@@ -12,11 +12,19 @@ public class LimsFly : MonoBehaviour
     private float alpha;
     private int limsNumber;
 
+    private Vector3 currentPosition;
+    private float parabolaVariable;
+
     // Use this for initialization
     void Start()
     {
         isFinish = false;
         alpha = 0;
+        parabolaVariable = -25;
+        if(this.GetComponent<CapsuleCollider>())
+        {
+            this.GetComponent<CapsuleCollider>().enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -25,8 +33,12 @@ public class LimsFly : MonoBehaviour
         if (alpha <= 1.0f)
             alpha += 0.01f;
         endPosition = targetPlayer.GetComponent<SCR_TradeLimb>().limbs[limsNumber].GetComponent<Transform>().position;
-        this.GetComponent<Transform>().position = Vector3.Lerp(startPosition, endPosition, alpha);
+        currentPosition.x = Mathf.Lerp(startPosition.x, endPosition.x, alpha);
+        currentPosition.z = Mathf.Lerp(startPosition.z, endPosition.z, alpha);
+        currentPosition.y = startPosition.y + parabolaVariable * alpha * alpha + (endPosition.y - parabolaVariable) * alpha;
+        this.GetComponent<Transform>().position = currentPosition;
 
+        //this.GetComponent<Transform>().position = Vector3.Lerp(startPosition, endPosition, alpha);
         if (alpha >= 1)
         {
             isFinish = true;

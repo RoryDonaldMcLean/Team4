3d38060fc8f09fba6b7472bfb30679dda0de63a4
Blast2Slay <38786844+Blast2Slay@users.noreply.github.com>
2018-06-21 16:23:10 +0100

    Storm/Lighting/Cutscene Controller
    Created a custom controller to tell the cutscenes to start stop and if they should repeat
    Created Custom particles for rain and lighting

---
+++ Robot/Assets/Scripts/Timeline/TimelineTriggerArea.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineTriggerArea : MonoBehaviour
+{
+
+    [Header("Component References")]
+    public TimelinePlaybackManager timelinePlaybackManager;
+
+    [Header("Settings")]
+    public string playerString = "Player1";
+
+
+    void OnTriggerEnter(Collider theCollision)
+    {
+        timelinePlaybackManager.PlayerEnteredZone();
+    }
+
+    void OnTriggerExit(Collider theCollision)
+    {
+        timelinePlaybackManager.PlayerExitedZone();
+    }
+}

2d2959b0cd2975abdff3de87c83b95b50e073d50
RoryDonaldMcLean <1300060@abertay.ac.uk>
2018-06-22 10:06:14 +0100

    Resolved Nekko merge conflicts
    Player prefabs, tags, scripts changed

--- Robot/Assets/Scripts/Player/LimsFly.cs
+++ Robot/Assets/Scripts/Player/LimsFly.cs
@@ -40,18 +40,11 @@ public class LimsFly : MonoBehaviour
         if (alpha <= 1.0f)
             alpha += 0.01f;
         endPosition = targetPlayer.GetComponent<SCR_TradeLimb>().limbs[limsNumber].GetComponent<Transform>().position;
-<<<<<<< HEAD
-        this.GetComponent<Transform>().position = Vector3.Lerp(startPosition, endPosition, alpha);
-        
-       //Rotates the object while it flys to the player
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, -900), Time.deltaTime * 1 );
-
-=======
         currentPosition.x = Mathf.Lerp(startPosition.x, endPosition.x, alpha);
         currentPosition.z = Mathf.Lerp(startPosition.z, endPosition.z, alpha);
         currentPosition.y = startPosition.y + parabolaVariable * alpha * alpha + (endPosition.y - parabolaVariable) * alpha;
         this.GetComponent<Transform>().position = currentPosition;
->>>>>>> origin/Neko
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, -900), Time.deltaTime * 1);
 
         //this.GetComponent<Transform>().position = Vector3.Lerp(startPosition, endPosition, alpha);
         if (alpha >= 1)

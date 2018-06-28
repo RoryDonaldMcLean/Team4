c78adb06839a7a1a60ab05b23151fda76fec0740
Blast2Slay <38786844+Blast2Slay@users.noreply.github.com>
2018-06-28 15:44:31 +0100

    ColorBlindview/CutsceneAdjust/Small Rain Cleanup
    Colour blind view added to show us how a colour blind player would view scene
    Adjusting of cutscenes to make them smoother

--- Robot/Assets/Scripts/Timeline/TimelineTriggerArea.cs
+++ Robot/Assets/Scripts/Timeline/TimelineTriggerArea.cs
@@ -9,24 +9,35 @@ public class TimelineTriggerArea : MonoBehaviour
     public TimelinePlaybackManager timelinePlaybackManager;
 
     [Header("Settings")]
-    public string playerString = "Player1";
-    public string playerString2 = "Player1";
+    public string playerString = "Crate";
+    public string playerString2 = "Crate2";
+
+    private int Collisions = 0;
 
 
     void OnTriggerEnter(Collider theCollision)
     {
-        if (theCollision.gameObject.name == "Crate" && theCollision.gameObject.name == "Crate2")
+       
+
+        if (theCollision.name.Contains("Crate") || theCollision.name.Contains("Crate2"))
+        {
+            Debug.Log("" + Collisions);
+            Collisions ++;
+        }
+
+        if (Collisions == 2)
         {
             timelinePlaybackManager.PlayerEnteredZone();
         }
-        else
-        {
-            timelinePlaybackManager.PlayerEnteredZone();
-        }
+        //else if (theCollision.name.Contains("Player") || theCollision.name.Contains("Player2"))
+        //{
+        //    timelinePlaybackManager.PlayerEnteredZone();
+        //}
     }
 
     void OnTriggerExit(Collider theCollision)
     {
         timelinePlaybackManager.PlayerExitedZone();
     }
+
 }

fc9bfb681a551cb71f3704ac281053b275e89f8a
RoryDonaldMcLean <1300060@abertay.ac.uk>
2018-08-07 14:31:28 +0100

    TradeLimb tidy up
    -trading limb functions have been tidied up to be more efficant
    -in scene 5 the light barrier was given the correct tag
    - toni's switch code has been chopped and updated

--- Robot/Assets/Scripts/Timeline/TimelineTriggerArea.cs
+++ Robot/Assets/Scripts/Timeline/TimelineTriggerArea.cs
@@ -6,58 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class TimelineTriggerArea : MonoBehaviour
 {
-    [Header("Component References")]
     public TimelinePlaybackManager timelinePlaybackManager;
-
-    [Header("Settings")]
-    public string playerString = "Crate";
-    public string playerString2 = "Crate2";
-
-    [Header("Switch Player Collision On")]
     public bool Switch = true;
-
-    [Header("Exit Level")]
-    public bool Exit = false;
-
-    private int Collisions = 0;
 
     void OnTriggerEnter(Collider theCollision)
     {
-        if (theCollision.name.Contains("Crate") || theCollision.name.Contains("Crate2"))
-        {
-            Debug.Log("" + Collisions);
-            Collisions ++;
-        }
-
-        if (Collisions == 2)
-        {
-            timelinePlaybackManager.PlayerEnteredZone();
-        }
-
+        
         if (Switch == true)
         {
             if (theCollision.name.Contains("Player") || theCollision.name.Contains("Player2") || theCollision.gameObject.layer.Equals(LayerMask.NameToLayer("PlayerLayer")))
             {
-                timelinePlaybackManager.PlayerEnteredZone();
                 timelinePlaybackManager.Switch = true;
             }
         }
-
-        if (Exit == true)
-        {
-            if (theCollision.name.Contains("Player") || theCollision.name.Contains("Player2"))
-            {
-                timelinePlaybackManager.PlayerEnteredZone();
-                
-            }
-        }
-
     }
-
-    void OnTriggerExit(Collider theCollision)
-    {
-        timelinePlaybackManager.PlayerExitedZone();
-    }
-
- 
 }

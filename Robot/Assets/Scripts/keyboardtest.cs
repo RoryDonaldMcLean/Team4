6d0f73be62415b45ea07e1fae1dae061b087b64c
xyskin <xyskin@163.com>
2018-08-08 16:23:11 +0100

    keyboard test

---
+++ Robot/Assets/Scripts/keyboardtest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyboardtest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("D");
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("W");
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("Up");
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Right");
+        }
+	}
+}

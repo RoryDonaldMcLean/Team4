cfb1f2e0f111ed9ea60201a0b106cc3801da6185
Paul Drauz-Brown <31356288+ImTethered@users.noreply.github.com>
2018-08-09 15:13:42 +0100

    Fixing chirp combining in chirp scripts

--- Robot/Assets/Scripts/Player/ChirpCollector.cs
+++ Robot/Assets/Scripts/Player/ChirpCollector.cs
@@ -2,41 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChirpCollector : MonoBehaviour 
+public class ChirpCollector : MonoBehaviour
 {
-	public int eventCount = 0;
-	public float timeLeft = 2.0f;
-	public bool startTimer = false;
-	public bool playEvent = false;
+    //public int eventCount = 0;
+    public bool playeroneChirped = false;
+    public bool playertwoChirped = false;
+    public float timeLeft = 1.0f;
+    public bool startTimer = false;
+    public bool playEvent = false;
 
-	bool addMovementCheck = false;
+    bool addMovementCheck = false;
 
-	// Use this for initialization
-	void Start () 
-	{
+    // Use this for initialization
+    void Start()
+    {
         AkSoundEngine.SetState("ChirpCombine", "Off");
+        playeroneChirped = false;
+        playertwoChirped = false;
     }
 
     // Update is called once per frame
-    void Update () 
-	{
-		//example of timer will be put elsewhere?
-		if (startTimer == true)
-		{
-			timeLeft -= Time.deltaTime;
-			if (eventCount >= 2)
-			{
-				playEvent = true;
-			}
-		}
+    void Update()
+    {
+        //example of timer will be put elsewhere?
+        if (startTimer == true)
+        {
+            timeLeft -= Time.deltaTime;
+            if (playeroneChirped == true && playertwoChirped == true)
+            {
+                playEvent = true;
+            }
+        }
 
-		if (timeLeft <= 0)
-		{
-			Debug.Log ("timer is at 0, play event");
-			startTimer = false;
-			timeLeft = 2.0f;
-			eventCount = 0;
-			FalseEvent ();
+        if (timeLeft <= 0)
+        {
+            Debug.Log("timer is at 0, play event");
+            startTimer = false;
+            timeLeft = 1.0f;
+            playeroneChirped = false;
+            playertwoChirped = false;
+            FalseEvent ();
 		}
 
 
@@ -56,8 +61,9 @@ public class ChirpCollector : MonoBehaviour
         //the event you want
         startTimer = false;
 		playEvent = false;
-		eventCount = 0;
-		timeLeft = 2.0f;
+        playeroneChirped = false;
+        playertwoChirped = false;
+        timeLeft = 1.0f;
 
 
         if ((this.GetComponent<LevelController>().currentLevel==0)&&(addMovementCheck == false))

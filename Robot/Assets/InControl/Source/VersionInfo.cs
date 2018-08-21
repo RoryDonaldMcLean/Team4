50dcb8358663bd6f0ce9b5f54cc6f3e989fc4d61
Paul Drauz-Brown <31356288+ImTethered@users.noreply.github.com>
2018-08-21 16:35:01 +0100

    Mixing

--- Robot/Assets/InControl/Source/VersionInfo.cs
+++ Robot/Assets/InControl/Source/VersionInfo.cs
@@ -29,9 +29,9 @@ namespace InControl
 				Minor = 4,
 				Patch = 4,
 //<<<<<<< HEAD
-				Build = 4150
+				Build = 4154
 //=======
-				//Build = 4092
+				//Build = 4096
 //>>>>>>> 0c05da32b7cbe2bab8985d6efc11a3b06d0a745c
 			};
 		}

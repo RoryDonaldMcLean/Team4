7c3050daf8f2edda6ee5fd9ba0d723431976efef
xyskin <xyskin@163.com>
2018-08-22 11:36:04 +0100

    add scriptableobject

---
+++ Robot/Assets/Scripts/ScriptableObject/TextData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextData")]
+public class TextData : ScriptableObject {
+    [TextArea]
+    public string[] Text;
+}

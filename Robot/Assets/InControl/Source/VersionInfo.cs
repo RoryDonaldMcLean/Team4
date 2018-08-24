c92d9fe8e75a9af04e6ec00e24dac1fcdd913ff5
xyskin <xyskin@163.com>
2018-08-24 12:11:15 +0100

    scene3 exit door

--- Robot/Assets/InControl/Source/VersionInfo.cs
+++ Robot/Assets/InControl/Source/VersionInfo.cs
@@ -29,9 +29,9 @@ namespace InControl
 				Minor = 4,
 				Patch = 4,
 //<<<<<<< HEAD
-				Build = 4373
+				Build = 4375
 //=======
-				//Build = 4315
+				//Build = 4317
 //>>>>>>> 0c05da32b7cbe2bab8985d6efc11a3b06d0a745c
 			};
 		}

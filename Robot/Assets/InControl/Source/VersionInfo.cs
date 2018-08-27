0dbabb199c371b5315e57471f9d4aaccda212c93
xyskin <xyskin@163.com>
2018-08-27 16:47:29 +0100

    scene 1 lollider

--- Robot/Assets/InControl/Source/VersionInfo.cs
+++ Robot/Assets/InControl/Source/VersionInfo.cs
@@ -29,9 +29,9 @@ namespace InControl
 				Minor = 4,
 				Patch = 4,
 //<<<<<<< HEAD
-				Build = 4523
+				Build = 4529
 //=======
-				//Build = 4465
+				//Build = 4471
 //>>>>>>> 0c05da32b7cbe2bab8985d6efc11a3b06d0a745c
 			};
 		}
